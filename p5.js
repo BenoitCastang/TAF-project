1b2bf307646dc09b1cf4de271b473d5a9201894d
BenoitCastang <castang.benoit@gmail.com>
2021-02-05 16:29:23 +0100

    update

--- p5.js
+++ p5.js
@@ -1,44 +1,79 @@
+// function setup() {
+//     createCanvas(800, 800);
+//     stroke(0, 255, 0);
+//     strokeWeight(0);
+//     pointX = 0;
+//     pointY = 0;
+//     sunX = 400;
+//     sunY = 800;
+//     sunHeight = 150;
+// }
+
+// function draw() {
+//     if (keyIsDown(LEFT_ARROW)) {
+//         background(200, 0, 50);
+//         pointX-=5;
+//         ellipse(pointX, pointY, 20, 20);
+//     }
+//     if (keyIsDown(RIGHT_ARROW)) {
+//         background(200, 0, 50);
+//         pointX+=5;
+//         ellipse(pointX, pointY, 20, 20);
+//     }
+//     if (keyIsDown(UP_ARROW)) {
+//         background(200, 0, 50);
+//         pointY-=5;
+//         ellipse(pointX, pointY, 20, 20);
+//     }
+//     if (keyIsDown(DOWN_ARROW)) {
+//         background(200, 0, 50);
+//         pointY+=5;
+//         ellipse(pointX, pointY, 20, 20);
+//     }
+//     if (pointX<0 || pointX>800 || pointY<0 || pointY>800) {
+//         pointX=width/2;
+//         pointY=height/2;
+//     }
+//     if (mouseButton) {
+//         if (sunY<(-75)) {
+//             sunY=800;
+//         } 
+//         strokeWeight(0);
+//         background(0, 0, 50);
+//         fill(0,0,50);
+//         rect(0, 600, 800, 200);
+//         fill(255,255,0);
+//         circle(sunX, sunY, 150);
+//         sunY = sunY-0.5;
+//     }
+//     if (mouseIsPressed) {
+//         sunY = sunY-4;
+//     }
+//     if (sunY<700) {
+//         fill('orange');
+//         rect(0, 0, 800, 600);
+//         fill('rgb(255,255,0)');
+//         circle(sunX, sunY, 150);
+//     }
+//     if (sunY<400) {
+//         fill('rgb(135,206,235)');
+//         rect(0, 0, 800, 600);
+//         fill('rgb(255,255,0)');
+//         circle(sunX, sunY, 150);
+//         rect(0, 600, 800, 200);
+//         fill('rgb(29,29,187)');
+
+//     }
+// } 
+
 function setup() {
     createCanvas(800, 800);
-    stroke(0, 255, 0);
-    strokeWeight(20);
-    // frameRate(1);
+    background(0,0,0,200);
+    stroke(255);
+    strokeWeight(1);
 }
 
-let x=200;
-let y=200;
 function draw() {
-    // background(200, 0, 50);
-    // ellipse(50,200,40,150);
-    // console.log('draw');
-    // point(100, 150);
-    // line(10, 50, 350, 50);
-    // circle(mouseX, mouseY, 200);
-    // triangle(200, 100, 400, 200, 200, 200);
-    // if (mouseIsPressed) {
-        //     if (mouseButton === LEFT) {
-    //         ellipse(mouseX, mouseY, mouseX, mouseY);
-    //         point()
-    //     }
-    // }
-    if (keyIsDown(LEFT_ARROW)) {
-        background(200, 0, 50);
-        x-=5;
-        point(x, y);
-    }
-    if (keyIsDown(RIGHT_ARROW)) {
-        background(200, 0, 50);
-        x+=5;
-        point(x, y);
-    }
-    if (keyIsDown(UP_ARROW)) {
-        background(200, 0, 50);
-        y-=5;
-        point(x, y);
-    }
-    if (keyIsDown(DOWN_ARROW)) {
-        background(200, 0, 50);
-        y+=5;
-        point(x, y);
-    }
-} 
+    noFill();
+    ellipse(mouseX, mouseY, 60, 60);
+}
